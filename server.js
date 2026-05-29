462d90a4398094fb2db3c6f158f2eeb90435127b
asapevilrocky-cmd <asapevilrocky@gmail.com>
2026-05-29 21:23:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -357,4 +357,4 @@ app.post('/send-order-sms', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server running on port ' + PORT));
+app.listen(PORT, '0.0.0.0', () => console.log('Server running on port ' + PORT));
